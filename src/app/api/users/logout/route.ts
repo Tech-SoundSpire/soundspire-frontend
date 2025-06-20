ec7ca16781889f223e19957fae975aaf52ebd805
killlercam <herosviral@gmail.com>
2025-06-20 14:30:42 +0530

    Done with signup and signin page

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -10,7 +10,15 @@ export async function GET( request: NextRequest) {
     response.cookies.set("token", "", {
       httpOnly: true,
       expires: new Date(0),
-      path: '/'
+      path: '/',
+      sameSite: "lax"
+    });
+    //clearing the user cookie
+    response.cookies.set("user", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      path: '/',
+      sameSite: "lax"
     });
 
     return response;
