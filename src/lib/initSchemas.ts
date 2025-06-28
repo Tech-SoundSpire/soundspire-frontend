3f54d7cc131eb0b476cef026e672b99619d20570
killlercam <herosviral@gmail.com>
2025-06-28 17:34:38 +0530

    Reverting to old code

--- src/lib/initSchemas.ts
+++ src/lib/initSchemas.ts
@@ -8,8 +8,8 @@ import "../models/UserVerification"
 export async function initSchemas() {
   try {
     //Syncing the tables based on the models
-    // await sequelize.sync({ force: true }); //Only if database change is needed during development mode
-    await sequelize.sync();
+    await sequelize.sync({ force: true }); //Only if database change is needed during development mode
+    // await sequelize.sync();//After database is changed enable this
     console.log("All models were synchronized successfully!");
 
   } catch (err) {
