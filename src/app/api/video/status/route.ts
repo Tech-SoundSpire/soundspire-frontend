4f01f1b002213a3462a08e11e0161e6fecb1addb
jishnu <jishnu@soundspire.online>
2026-03-17 21:40:31 +0530

    fix: update to use correct env variables.

--- src/app/api/video/status/route.ts
+++ src/app/api/video/status/route.ts
@@ -4,8 +4,8 @@ import { S3Client, HeadObjectCommand, ListObjectsV2Command } from '@aws-sdk/clie
 const s3 = new S3Client({
   region: 'ap-south-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || process.env.BUCKET_AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || process.env.BUCKET_AWS_SECRET_ACCESS_KEY || '',
   },
 });
 
